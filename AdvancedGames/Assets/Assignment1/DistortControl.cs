edcaf79540080c99cb9e3f47bda2075969088e9a
Cole <coleaaronmueller@gmail.com>
2017-10-16 18:30:21 -0400

    Folder Organization

---
+++ AdvancedGames/Assets/Assignment1/DistortControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistortControl : MonoBehaviour {
+
+    public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+	rend = this.GetComponent<Renderer>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float ocellate = Mathf.PingPong(Time.time, 1000);
+        rend.material.SetFloat("_DistortMod", ocellate);
+		
+	}
+}
