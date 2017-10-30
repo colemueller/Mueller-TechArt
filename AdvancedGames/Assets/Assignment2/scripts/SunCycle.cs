feb262b415ef9a84a9f1ba95ff4d18a8f6f0aa13
Gabe <gabrielgdavis@gmail.com>
2017-10-29 20:21:33 -0400

    Fixed cycle script
    baby got back

--- AdvancedGames/Assets/Assignment2/scripts/SunCycle.cs
+++ AdvancedGames/Assets/Assignment2/scripts/SunCycle.cs
@@ -11,7 +11,7 @@ public class SunCycle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.RotateAround(Vector3.zero, Vector3.right, 10f*Time.deltaTime);
+		transform.RotateAround(Vector3.zero, Vector3.back, 10f*Time.deltaTime);
 		transform.LookAt(Vector3.zero);
 	}
 }
