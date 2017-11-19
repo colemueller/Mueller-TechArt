f09a6211285e7b717af1fee1d2427b4c6daafdb3
Cole <coleaaronmueller@gmail.com>
2017-11-19 16:05:51 -0500

    Added placeholder water plane
    Did some movement tweaks

---
+++ AdvancedGames/Assets/FinalAssignment/Cole/WaterTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTrigger : MonoBehaviour {
+
+    public GameObject fish;
+
+	// Use this for initialization
+	void Start () {
+        fish.GetComponent<Rigidbody>().useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        fish.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        fish.GetComponent<Rigidbody>().useGravity = false;
+        fish.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+}
