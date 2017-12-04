7245f2d1e1ae262a7064cbc00df0b532ff56a213
Unknown <gabrielgdavis@gmail.com>
2017-12-04 00:13:25 -0500

    Updates to look, kelp sway, kelp spawn

---
+++ AdvancedGames/Assets/FinalAssignment/Gabe/KelpMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KelpMove : MonoBehaviour
+{
+    //public Transform target;
+    public float smoothTime = 4F;
+    private float yVelocity = 0.0F;
+    private static bool Direction = false; //up = true, down = false
+
+    // Update is called once per frame
+    void Update()
+    {
+        float curAcc = transform.GetComponent<Cloth>().externalAcceleration.x;
+
+        if (Direction == true)
+        {
+            float newPosition = Mathf.SmoothDamp(curAcc, 0.4F, ref yVelocity, smoothTime);
+            transform.GetComponent<Cloth>().externalAcceleration = new Vector3(newPosition, 0, (newPosition*-1));
+
+            if (curAcc >= 0.38F)
+            {
+                Direction = false;
+            }
+        }
+
+        if (Direction == false)
+        {
+            float newPosition = Mathf.SmoothDamp(curAcc, -0.4F, ref yVelocity, smoothTime);
+            transform.GetComponent<Cloth>().externalAcceleration = new Vector3(newPosition, 0, (newPosition*-1));
+
+            if (curAcc <= -0.38F)
+            {
+                Direction = true;
+            }
+        }
+
+       
+    }
+}
---
+++ AdvancedGames/Assets/FinalAssignment/Gabe/OldKelpSpawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldKelpSpawn : MonoBehaviour {
+    SkinnedMeshRenderer rend;
+    private float yVelocity = 0.0F;
+    private float smoothTime = 1.0F;
+    bool doDis = true;
+
+    private void Start()
+    {
+        
+        rend = GetComponent<SkinnedMeshRenderer>();
+        rend.material.shader = Shader.Find("Custom/Dissolve Shader");
+        //rend.material.shader = Shader.Find("Dissolve Shader");
+        StartCoroutine(setZero(8));
+    }
+
+    // Use this for initialization
+    void Update () {
+
+        if (doDis)
+        {
+            float temp = rend.material.GetFloat("_DissolveAmount");
+
+            float dissolve = Mathf.SmoothDamp(temp, -0.1F, ref yVelocity, smoothTime);
+
+            //float dissolve = 0;
+            rend.material.SetFloat("_DissolveAmount", dissolve);
+            
+        }
+    }
+
+    IEnumerator setZero(float time)
+    {
+        yield return new WaitForSeconds(time);
+        doDis = false;
+        rend.material.SetFloat("_DissolveAmount", 0);
+        
+    }
+}
